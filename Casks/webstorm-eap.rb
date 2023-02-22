efc9a406cd871ac164509be1210eeb610cf1f0fd
dx.wang <wdx@me.com>
2023-02-22 16:56:53 +0800

    Update webstorm-eap.rb
    update to 231.7515.16

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,7 +1,7 @@
 cask "webstorm-eap" do
   arch arm: "-aarch64"
 
-  version "2023.1,231.6890.13"
+  version "2023.1,231.7515.16"
   sha256 arm: "c4e03a7b71c5ee4f4e4c3c0115ceccabbaaaef35970fcc0dbb6cae63ffc6d7cd",
          intel: "cfd0c67414e6e37761620679a4d9dec4a06038d75c31917984638e8eb392253c"
 
