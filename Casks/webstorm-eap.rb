9d7b99936636fa0b4321174795b1c6791566a7fb
dx.wang <wdx@me.com>
2023-06-01 13:38:18 +0800

    Update webstorm-eap.rb
    2023.2,232.6095.12

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,9 +1,9 @@
 cask "webstorm-eap" do
   arch arm: "-aarch64"
 
-  version "2023.1,231.8109.51"
-  sha256 arm: "37218654af0162cded9b9b0e8d280faa74aa50c6dcf5fa2889b2e9dd9d7a53c3",
-         intel: "f1582b42022f5012be34441b1f3e88935739bc4bf4dbd489b5eddb069272d01c"
+  version "2023.2,232.6095.12"
+  sha256 arm: "f4991a026191c52014386e0c9cd013b4872fcb6a983bf9a299eb048af15e0333",
+         intel: "4b76c4057be25702c71a950802a3bb284aa18dea53ca0629fba3d9290c276ee2"
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.last}#{arch}.dmg"
   name "WebStorm-EAP"
